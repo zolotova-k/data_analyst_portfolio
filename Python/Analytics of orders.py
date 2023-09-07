e97e076f5014a8cc16d4953587c31bd712b5124c
Ekaterina Zolotova <143661206+zolotova-k@users.noreply.github.com>
2023-09-07 14:38:49 +0200

    Update Analytics of orders.py

--- Python/Analytics of orders.py
+++ Python/Analytics of orders.py
@@ -21,55 +21,54 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
-#import data
+  #import data:
 df= pd.read_excel('example_orders.xlsx')
 
-#enter machine model (here: ABC100)
+  #enter machine model (here: ABC100):
 df=df[df['ORDERCODE'].str.startswith('ABC100')]
 
-#enter machine code (here: ABC)
- df=df[df.columns[pd.Series(df.columns).str.startswith('ABC')]]
+  #enter machine code (here: ABC):
+df=df[df.columns[pd.Series(df.columns).str.startswith('ABC')]]
  
 result2 = pd.DataFrame(columns=['Ordercode','Feature', 'Options', 'Count'])
-#a table to count all of the feature options_
+      #a table to count all of the feature options
 result1 = pd.DataFrame(columns=['Feature1', 'Options1', 'Relative'])
-#a table to save the relative frequencies of the values
+      #a table to save the relative frequencies of the values
 result3 = pd.DataFrame(columns=['Feature', 'Options', 'Relative without *'])
-#a table to calculate the relative frequencies of the values excluding *
+      #a table to calculate the relative frequencies of the values excluding *
 result = pd.DataFrame(columns=['Ordercode','Feature', 'Options','Count','Relative','Relative without *'])
-#a table for the final result
+      #a table for the final result
 
 for column in df.columns:
-    
-   value_counts = df[column].value_counts()
-   #value_counts() function returns object containing counts of unique values
+      value_counts = df[column].value_counts()
+      #value_counts() function returns object containing counts of unique values
    for idx, (value, count) in enumerate(value_counts.items()):
-     result2 = pd.concat([result2, pd.DataFrame({'Ordercode': a,'Feature': column, 'Options': value, 'Count': count}, index=[0])], ignore_index=True)
+      result2 = pd.concat([result2, pd.DataFrame({'Ordercode': a,'Feature': column, 'Options': value, 'Count': count}, index=[0])], ignore_index=True)
        
- value_counts = df[column].value_counts(normalize=True)
- #the function "value_counts(normalize=True)" returns the relative frequencies of the values
-<br /> for idx, (value, count) in enumerate(value_counts.items()):
-<br />     result1 = pd.concat([result1, pd.DataFrame({'Feature1': column, 'Options1': value, 'Relative': count}, index=[0])], ignore_index=True) 
-<br /> 
-<br /> df1 = df.copy()
-<br />       
-<br /> for column in df1.columns:
-<br />     df1[column] = df1[column].replace(['`*`'], None)
-<br />     value_counts = (df1[column]).value_counts(normalize=True)
-<br />    for idx, (value, count) in enumerate(value_counts.items()):
-<br />      result3= pd.concat([result3, pd.DataFrame({'Feature': column, 'Options': value, 'Relative without `*`': count}, index=[0])], ignore_index=True)         
-<br /> 
-<br /> result3.loc[:, 'Relative without `*`'] = result3['Relative without *'].map('{:.2f}'.format)     
-<br /> result33 = result3.replace(np.nan, '', regex=True)      
-<br /> result = pd.concat([result2, result1], axis=1)     
-<br /> result = pd.merge(result, result33, on=[ 'Feature', 'Options'], how="left")
-<br /> result.loc[:, 'Relative'] = result['Relative'].map('{:.2f}'.format)
-<br /> result=result.fillna("")
-<br /> 
-<br /> with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 1000):
-<br />     print(result[['Ordercode','Feature', 'Options', 'Count', 'Relative','Relative without `*`']])
-<br /> 
-<br /> _#export data to excel_
-<br /> with pd.ExcelWriter('Output.xlsx', mode='a', if_sheet_exists='overlay') as writer: 
-<br /> _#change the name of the Excel sheet here if needed_
-<br />     result[['Ordercode','Feature', 'Options', 'Count', 'Relative','Relative without `*`']].to_excel(writer, sheet_name='sheet1')
+value_counts = df[column].value_counts(normalize=True)
+#the function "value_counts(normalize=True)" returns the relative frequencies of the values
+for idx, (value, count) in enumerate(value_counts.items()):
+             result1 = pd.concat([result1, pd.DataFrame({'Feature1': column, 'Options1': value, 'Relative': count}, index=[0])], ignore_index=True) 
+ 
+df1 = df.copy()
+       
+for column in df1.columns:
+   df1[column] = df1[column].replace(['`*`'], None)
+   value_counts = (df1[column]).value_counts(normalize=True)
+for idx, (value, count) in enumerate(value_counts.items()):
+               result3= pd.concat([result3, pd.DataFrame({'Feature': column, 'Options': value, 'Relative without *': count}, index=[0])], ignore_index=True)         
+ 
+result3.loc[:, 'Relative without *'] = result3['Relative without *'].map('{:.2f}'.format)     
+result33 = result3.replace(np.nan, '', regex=True)      
+result = pd.concat([result2, result1], axis=1)     
+result = pd.merge(result, result33, on=[ 'Feature', 'Options'], how="left")
+result.loc[:, 'Relative'] = result['Relative'].map('{:.2f}'.format)
+result=result.fillna("")
+ 
+with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 1000):
+      print(result[['Ordercode','Feature', 'Options', 'Count', 'Relative','Relative without *']])
+ 
+#export data to excel:
+with pd.ExcelWriter('Output.xlsx', mode='a', if_sheet_exists='overlay') as writer:
+      #change the name of the Excel sheet here if needed
+      result[['Ordercode','Feature', 'Options', 'Count', 'Relative','Relative without `*`']].to_excel(writer, sheet_name='sheet1')
